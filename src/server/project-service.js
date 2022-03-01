307d6d1c73352df5018f1829614934573b145559
dgjungleP <dgJungle_c@163.com>
2022-03-01 09:11:18 +0800

    change

--- src/server/project-service.js
+++ src/server/project-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseApi = "http://localhost:8080/open-api/";
+const baseApi = "http://172.16.171.106:8080/open-api/";
 function getProject(request) {
   return axios.get(
     `${baseApi}projects?year=${request.year}&month=${request.month}&history=${request.history}&group=${request.group}`
