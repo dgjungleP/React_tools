a0eb5730fdeeff03567dcd0e37fd785cc091cdc4
dgjungleP <dgJungle_c@163.com>
2022-04-28 18:29:11 +0800

    holiday

--- src/compoments/config/holiday.jsx
+++ src/compoments/config/holiday.jsx
@@ -3,21 +3,34 @@ import "antd/dist/antd.css";
 import "./holiday.css";
 
 import {
-  getSystemConfig,
   getHoliday,
+  getSystemConfig,
   getUserInfo,
   updateHoliday,
   updateUserInfo,
 } from "../../server/project-service";
-import { Col, PageHeader, Row, Select, Tabs } from "antd";
-import { func } from "prop-types";
-import { loadConfigFromFile } from "vite";
+import {
+  Button,
+  Card,
+  Col,
+  DatePicker,
+  Descriptions,
+  InputNumber,
+  List,
+  PageHeader,
+  Row,
+  Select,
+  Spin,
+  Tabs,
+} from "antd";
+import Modal from "antd/lib/modal/Modal";
+import Input from "antd/lib/input/Input";
+import moment from "moment";
 const { TabPane } = Tabs;
 
 const { Option } = Select;
 function HolidayConfig(props) {
   const [systemConfig, setSystemConfig] = useState([]);
-
   useEffect(() => {
     getSystemConfig().then((response) => {
       setSystemConfig(response.data);
@@ -35,15 +48,21 @@ function Ascription(props) {
   const config = props.config;
   const [userInfo, setUserInfo] = useState([]);
   const [currentSystem, setCunrrentSystem] = useState();
-  useEffect(() => {
+  const [loading, setLoading] = useState();
+  const freshData = () => {
+    setLoading(true);
     Promise.all([...config.map((system) => getUserInfo(system.id))]).then(
       (res) => {
         const userInfoList = res.map((res) => {
           return { system: (res.data[0] || {}).systemId, data: res.data };
         });
         setUserInfo(userInfoList);
+        setLoading(false);
       }
     );
+  };
+  useEffect(() => {
+    freshData();
   }, [config]);
   const changeTab = (key) => {
     setCunrrentSystem(key);
@@ -55,30 +74,32 @@ function Ascription(props) {
         title="Ascription"
         subTitle="Manage your Place of ownership"
       ></PageHeader>
-      <Tabs
-        type="card"
-        onChange={changeTab}
-        style={{ backgroundColor: "white" }}
-      >
-        {config.map((system) => {
-          return (
-            <TabPane tab={system.systemName} key={system.id}>
-              <Row gutter={16}>
-                {userInfo
-                  .filter((data) => data.system == system.id)
-                  .map((data) => {
-                    return (
-                      <SpanWithSelect
-                        key={data.system}
-                        userInfo={data.data}
-                      ></SpanWithSelect>
-                    );
-                  })}
-              </Row>
-            </TabPane>
-          );
-        })}
-      </Tabs>
+      <Spin spinning={loading}>
+        <Tabs
+          type="card"
+          onChange={changeTab}
+          style={{ backgroundColor: "white" }}
+        >
+          {config.map((system) => {
+            return (
+              <TabPane tab={system.systemName} key={system.id}>
+                <Row gutter={16}>
+                  {userInfo
+                    .filter((data) => data.system == system.id)
+                    .map((data) => {
+                      return (
+                        <SpanWithSelect
+                          key={data.system}
+                          userInfo={data.data}
+                        ></SpanWithSelect>
+                      );
+                    })}
+                </Row>
+              </TabPane>
+            );
+          })}
+        </Tabs>
+      </Spin>
     </>
   );
 }
@@ -121,13 +142,142 @@ function SpanWithSelect(props) {
 }
 
 function Holiday(props) {
+  const [loading, setLoading] = useState();
+  const [holidayList, setHolidayList] = useState([]);
+  const [currentHoliday, setCurrentHoliday] = useState({});
+  const [modalShow, setModalShow] = useState();
+  const freshData = () => {
+    setLoading(true);
+    getHoliday().then((res) => {
+      setHolidayList(res.data);
+      setLoading(false);
+    });
+  };
+  const editHoliday = (holiday) => {
+    setCurrentHoliday(holiday);
+    setModalShow(true);
+  };
+  const handleOk = () => {
+    setModalShow(false);
+    console.log(currentHoliday);
+    setCurrentHoliday(currentHoliday);
+    updateHoliday(currentHoliday).then((res) => {
+      freshData();
+    });
+  };
+  const handleCancel = () => {
+    setModalShow(false);
+    setCurrentHoliday({});
+  };
+  const handleCHange = (value) => {
+    const newHolidayIngfo = { ...currentHoliday, ...value };
+    setCurrentHoliday(newHolidayIngfo);
+  };
+  useEffect(() => {
+    freshData();
+  }, [currentHoliday]);
   return (
     <>
       <PageHeader
         className="site-page-header"
         title="Holiday"
         subTitle="Manage Holiday"
+        extra={[
+          <Button onClick={() => editHoliday({})} key="1" ghost type="primary">
+            New
+          </Button>,
+        ]}
       ></PageHeader>
+      <Spin spinning={loading}>
+        <List
+          style={{ backgroundColor: "white" }}
+          dataSource={holidayList}
+          renderItem={(item) => {
+            return (
+              <List.Item
+                actions={[
+                  <a key="list-loadmore-edit" onClick={() => editHoliday(item)}>
+                    edit
+                  </a>,
+                ]}
+              >
+                <Descriptions title={item.name} style={{ paddingLeft: 16 }}>
+                  <Descriptions.Item label="Date">
+                    {moment(item.dateInfo).format("MM-DD")}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Duration">
+                    {item.duration}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Ascription">
+                    {item.ascription}
+                  </Descriptions.Item>
+                </Descriptions>
+              </List.Item>
+            );
+          }}
+        ></List>
+      </Spin>
+      <Modal
+        visible={modalShow}
+        title="Holiday"
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Row gutter={16} align="middle">
+          <Col span={6} style={{ display: "flex", justifyContent: "end" }}>
+            <span>Name</span>:
+          </Col>
+          <Col>
+            <Input
+              value={currentHoliday.name}
+              onChange={(value) => handleCHange({ name: value.target.value })}
+            ></Input>
+          </Col>
+        </Row>
+        <Row gutter={16} align="middle">
+          <Col span={6} style={{ display: "flex", justifyContent: "end" }}>
+            <span>Date</span>:
+          </Col>
+          <Col>
+            <DatePicker
+              value={
+                currentHoliday.dateInfo ? moment(currentHoliday.dateInfo) : null
+              }
+              format={"MM-DD"}
+              onChange={(value) =>
+                handleCHange({ dateInfo: value.format("yyyy-MM-DD") })
+              }
+            ></DatePicker>
+          </Col>
+        </Row>
+        <Row gutter={16} align="middle">
+          <Col span={6} style={{ display: "flex", justifyContent: "end" }}>
+            <span>Duration</span>:
+          </Col>
+          <Col>
+            <InputNumber
+              value={currentHoliday.duration}
+              onChange={(value) => handleCHange({ duration: value })}
+            ></InputNumber>
+          </Col>
+        </Row>
+        <Row gutter={16} align="middle">
+          <Col span={6} style={{ display: "flex", justifyContent: "end" }}>
+            <span>Ascription</span>:
+          </Col>
+          <Col>
+            <Select
+              value={currentHoliday.ascription}
+              style={{ width: 80 }}
+              onChange={(value) => handleCHange({ ascription: value })}
+            >
+              <Option value="CD">CD</Option>
+              <Option value="TC">TC</Option>
+              <Option value="US">US</Option>
+            </Select>
+          </Col>
+        </Row>
+      </Modal>
     </>
   );
 }
