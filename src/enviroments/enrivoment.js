ddb40887ce486a6cbb8f41ff21291028471d1d01
dgjungleP <dgJungle_c@163.com>
2023-01-06 13:38:14 +0800

    修改接口

---
+++ src/enviroments/enrivoment.js
@@ -0,0 +1,6 @@
+export const enviroment = {
+  // BASE_URL: "http://172.16.171.106:8080/open-api/",
+  BASE_URL: "https://apis.newegg.org/plan/openapi/",
+
+  // BASE_URL: "http://localhost:8080/open-api/",
+};
