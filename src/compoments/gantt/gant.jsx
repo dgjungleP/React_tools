490dcfc069d740b77b3e5e1da2c5763ec294f618
dgjungle_c <dgjungle_c@163.com>
2023-03-22 10:25:37 +0800

    change icon

--- src/compoments/gantt/gant.jsx
+++ src/compoments/gantt/gant.jsx
@@ -128,7 +128,7 @@ function ReleaseTable(props) {
   const systemConfig = props.systemConfig;
   const [searchText, updateSearchText] = useState("");
   const [searchedColumn, updateSearchedColumn] = useState("");
-  const autoTetingTag = ["None", "Plan", "Doing", "Done"];
+  const autoTetingTag = ["None", "Plan", "Doing", "Done", "Ignore"];
 
   const getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
@@ -956,22 +956,35 @@ function formatter(i) {
         >
           {(record[i] || "").split("-")[0]}
           {tag && tag != "None" ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              strokeWidth={2}
-              width={15}
-              color={getColor(tag)}
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-                clipRule="evenodd"
-              />
-              <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-            </svg>
+            tag == "Ignore" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                strokeWidth={2}
+                width={15}
+                color={getColor(tag)}
+                className="w-6 h-6"
+              >
+                <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM20.25 5.507v11.561L5.853 2.671c.15-.043.306-.075.467-.094a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93zM3.75 21V6.932l14.063 14.063L12 18.088l-7.165 3.583A.75.75 0 013.75 21z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                strokeWidth={2}
+                width={15}
+                color={getColor(tag)}
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )
           ) : (
             ""
           )}
@@ -989,6 +1002,9 @@ function getColor(tag) {
   }
   if (tag == "Done") {
     return "#5c940d";
+  }
+  if (tag == "Ignore") {
+    return "#495057";
   }
 }
 
